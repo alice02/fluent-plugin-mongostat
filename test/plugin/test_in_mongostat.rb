ecec4445083b9e4c9aea0e459c256e009894bc52
Kouta Asai <asai.kota@nifty.co.jp>
2017-02-13 18:12:24 +0900

    [add] discover option test

--- test/plugin/test_in_mongostat.rb
+++ test/plugin/test_in_mongostat.rb
@@ -1,10 +1,12 @@
 require 'helper'
 
 class MongostatInputTest < Minitest::Test
+  extend Minitest::Spec::DSL
 
   def setup
     Fluent::Test.setup
   end
+
 
   CONFIG = %[
   ]
@@ -26,7 +28,7 @@ class MongostatInputTest < Minitest::Test
 
   def test_configure
     assert_raises(Fluent::ConfigError) do
-      d = create_driver CONFIG, 'test', FALSE
+      d = create_driver CONFIG, 'test', false
     end
 
     d = create_driver %[
@@ -44,11 +46,24 @@ class MongostatInputTest < Minitest::Test
     assert_equal 30, d.instance.refresh_interval
   end
 
-  def test_parse_line
-    raw_line = '{"localhost:27017":{"arw":"1|0","command":"2|0","conn":"1","delete":"*4","dirty":"0.1%","flushes":"2","getmore":"1","insert":"*3","net_in":"158b","net_out":"44.7k","qrw":"3|1","query":"*10","res":"62.0M","time":"06:32:22","update":"*1","used":"0.3%","vsize":"265M"}}'
+  let(:mongostat_output) {
+    '{"localhost:27017":{"arw":"1|0","command":"2|0","conn":"1","delete":"*4","dirty":"0.1%",' +
+      '"flushes":"2","getmore":"1","insert":"*3","net_in":"158b","net_out":"44.7k","qrw":"3|1",' +
+      '"query":"*10","res":"62.0M","time":"06:32:22","update":"*1","used":"0.3%","vsize":"265M"}}'
+  }
 
+  let(:mongostat_output_with_discover) {
+    '{"host1":{"ar|aw":"0|0","command":"9|0","conn":"32","delete":"*0","flushes":"0","getmore":"1",' +
+      '"host":"host1","insert":"*0","netIn":"793b","netOut":"44.0k","qr|qw":"0|0","query":"*0",' +
+      '"repl":"PRI","res":"1.08G","set":"set1","time":"11:26:11","update":"*0","vsize":"265M"},' +
+      '"host2":{"ar|aw":"0|0","command":"15|0","conn":"42","delete":"*0","flushes":"0","getmore":"0",' +
+      '"host":"host2","insert":"*0","netIn":"3.90k","netOut":"89.7k","qr|qw":"0|0","query":"23",' +
+      '"repl":"SEC","res":"994M","set":"set1","time":"11:26:11","update":"*0","vsize":"265M"}}'
+  }
+
+  def test_parse_line
     d = create_driver
-    parsed_hash = d.instance.parse_line raw_line
+    parsed_hash = d.instance.parse_line mongostat_output
     assert_equal parsed_hash['arw'], {'ar'=>'1', 'aw'=>'0'}
     assert_equal parsed_hash['command'], '2'
     assert_equal parsed_hash['conn'], '1'
@@ -65,6 +80,28 @@ class MongostatInputTest < Minitest::Test
     assert_equal parsed_hash['time'], '06:32:22'
     assert_equal parsed_hash['update'], '1'
     assert_equal parsed_hash['used'], '0.3%'
+    assert_equal parsed_hash['vsize'], '265M'
+  end
+
+  def test_parse_line_with_discover
+    d = create_driver
+    parsed_hash = d.instance.parse_line mongostat_output_with_discover
+    assert_equal parsed_hash['arw'], {'ar'=>'0', 'aw'=>'0'}
+    assert_equal parsed_hash['command'], '9'
+    assert_equal parsed_hash['conn'], '32'
+    assert_equal parsed_hash['delete'], '0'
+    assert_equal parsed_hash['flushes'], '0'
+    assert_equal parsed_hash['getmore'], '1'
+    assert_equal parsed_hash['insert'], '0'
+    assert_equal parsed_hash['netIn'], '793b'
+    assert_equal parsed_hash['netOut'], '44.0k'
+    assert_equal parsed_hash['qrw'], {'qr'=>'0', 'qw'=>'0'}
+    assert_equal parsed_hash['query'], '0'
+    assert_equal parsed_hash['repl'], 'PRI'
+    assert_equal parsed_hash['res'], '1.08G'
+    assert_equal parsed_hash['set'], 'set1'
+    assert_equal parsed_hash['time'], '11:26:11'
+    assert_equal parsed_hash['update'], '0'
     assert_equal parsed_hash['vsize'], '265M'
   end
 
